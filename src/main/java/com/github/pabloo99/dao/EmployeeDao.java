756443c76f1a7a69146c3db3fa03a2d7d7dc6583
SebastianJaroszek <sebastian_jaroszek@emstories.pl>
2018-01-30 18:58:28 +0100

    add .hbm and .cfg files into resources

--- src/main/java/com/github/pabloo99/dao/EmployeeDao.java
+++ src/main/java/com/github/pabloo99/dao/EmployeeDao.java
@@ -1,4 +1,19 @@
 package com.github.pabloo99.dao;
 
+import com.github.pabloo99.connection.HibernateUtil;
+import com.github.pabloo99.entity.Employee;
+import org.hibernate.Session;
+
+import java.util.List;
+
 public class EmployeeDao {
+
+    public List<Employee> findAll(){
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        
+
+        return null;
+    }
+
 }
--- src/main/java/com/github/pabloo99/entity/Employee.java
+++ src/main/java/com/github/pabloo99/entity/Employee.java
@@ -1,4 +1,24 @@
 package com.github.pabloo99.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@NoArgsConstructor
+@Data
 public class Employee {
+
+    private int employeeId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private Date hireDate;
+    private String jobId;
+    private double salary;
+    private double commissionPct;
+    private int managerId;
+    private int departmentId;
 }
